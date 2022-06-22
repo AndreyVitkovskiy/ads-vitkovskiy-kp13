99e9d6738ae8cab1d9d34f9c0c1e8acf7f66a56c
AndreyVitkovskiy <92927154+AndreyVitkovskiy@users.noreply.github.com>
2022-06-22 22:50:16 +0300

    Add files via upload
    Лабораторна робота № 6 завершена

--- Program.cs
+++ Program.cs
@@ -1,64 +1,144 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Lab5
+namespace Lab6
 {
-    class Program
+    public class DoublyNode<T>
     {
-        //метод обміну елементів
-        static void Swap(ref int e1, ref int e2)
+        public DoublyNode(T data)
         {
-            var temp = e1;
-            e1 = e2;
-            e2 = temp;
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+
+    public class Deque<T> : IEnumerable<T>  
+    {
+        DoublyNode<T> head; 
+        DoublyNode<T> tail; 
+        int count;  
+
+        public void AddLast(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+        public void AddFirst(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+            DoublyNode<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
+        }
+        public T RemoveFirst()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = head.Data;
+            if (count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                head = head.Next;
+                head.Previous = null;
+            }
+            count--;
+            return output;
+        }
+        public T RemoveLast()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = tail.Data;
+            if (count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                tail = tail.Previous;
+                tail.Next = null;
+            }
+            count--;
+            return output;
+        }
+        public T First
+        {
+            get
+            {
+                if (IsEmpty)
+                    throw new InvalidOperationException();
+                return head.Data;
+            }
+        }
+        public T Last
+        {
+            get
+            {
+                if (IsEmpty)
+                    throw new InvalidOperationException();
+                return tail.Data;
+            }
         }
 
-        //сортування змішуванням
-        static int[] ShakerSort(int[] array)
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
         {
-            for (var i = 0; i < array.Length / 2; i++)
-            {
-                var swapFlag = false;
-                for (var j = i; j < array.Length - i - 1; j++)
-                {
-                    if (array[j] > array[j + 1])
-                    {
-                        Swap(ref array[j], ref array[j + 1]);
-                        swapFlag = true;
-                    }
-                }
-
-                for (var j = array.Length - 2 - i; j > i; j--)
-                {
-                    if (array[j - 1] > array[j])
-                    {
-                        Swap(ref array[j - 1], ref array[j]);
-                        swapFlag = true;
-                    }
-                }
-
-                if (!swapFlag)
-                {
-                    break;
-                }
-            }
-
-            return array;
+            head = null;
+            tail = null;
+            count = 0;
         }
 
-        static void Main(string[] args)
+        public bool Contains(T data)
         {
-            Console.WriteLine("Сортування змішуванням");
-            Console.Write("Введіть елементи масиву: ");
-            var parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
-            var array = new int[parts.Length];
-            for (int i = 0; i < parts.Length; i++)
+            DoublyNode<T> current = head;
+            while (current != null)
             {
-                array[i] = Convert.ToInt32(parts[i]);
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
             }
+            return false;
+        }
 
-            Console.WriteLine("Впорядкований масив: {0}", string.Join(", ", ShakerSort(array)));
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
 
-            Console.ReadLine();
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+        public void Deque<T> Main(string[] args)
+        {
+            
         }
     }
 }
